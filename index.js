1d69dfbe2687a8b6b0750da37d4e8c7419073a15
shobhitsaha <79324583+shobhitsaha@users.noreply.github.com>
2024-06-12 22:55:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import userRoutes from "./routes/users.js";
 import relationshipRoutes from "./routes/relationships.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 import multer from "multer";
 import path from "path";
 import { fileURLToPath } from "url";
@@ -26,7 +27,10 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Credentials", true);
   next();
 });
-app.use(express.json());
+// app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(
   cors({
     origin: "http://localhost:3000",
