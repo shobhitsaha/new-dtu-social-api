0b567fd27dc0cba2a5a6529d8cb819726c06be16
shobhitsaha <79324583+shobhitsaha@users.noreply.github.com>
2024-06-12 23:24:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,11 +31,7 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-  })
-);
+app.use(cors())
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
